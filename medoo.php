7903b57d5ad337b59bd6c4cd04dc57b9d9039039
Catfan <catfan.me@gmail.com>
2014-06-06 22:33:32 +0800

    [fix] Improve fn_quote detection

--- medoo.php
+++ medoo.php
@@ -207,7 +207,7 @@ class medoo
 
 	protected function fn_quote($column, $string)
 	{
-		return (strpos($column, '#') === 0 && preg_match('/^[A-Z0-9\_]*\(.*\)$/', $string)) ?
+		return (strpos($column, '#') === 0 && preg_match('/^[A-Z0-9\_]*\([^)]*\)$/', $string)) ?
 
 			$string :
 
