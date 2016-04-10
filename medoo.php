e893cb203e9fce866317571c59372ba5d5917ebc
Catfan <catfan.me@gmail.com>
2016-04-10 18:46:30 +0800

    [fix] Fix SQL wildcard syntax bug

--- medoo.php
+++ medoo.php
@@ -344,15 +344,7 @@ class medoo
 							$item = strval($item);
 							$suffix = mb_substr($item, -1, 1);
 
-							if ($suffix === '_')
-							{
-								$item = substr_replace($item, '%', -1);
-							}
-							elseif ($suffix === '%')
-							{
-								$item = '%' . substr_replace($item, '', -1, 1);
-							}
-							elseif (preg_match('/^(?!%).+(?<!%)$/', $item))
+							if (preg_match('/^(?!(%|\[|_])).+(?<!(%|\]|_))$/', $item))
 							{
 								$item = '%' . $item . '%';
 							}
