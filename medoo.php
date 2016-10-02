102a622dab1a18b9c60f00feeeb3fe3e86ae0e1b
Andrius Gecius <andrius.gecius@gmail.com>
2016-10-02 17:06:25 +0200

    little change

--- medoo.php
+++ medoo.php
@@ -208,10 +208,10 @@ class Medoo
 
 		if (isset($column_match[ 2 ], $column_match[ 3 ]))
 		{
-			return '"' . $this->prefix . $column_match[ 2 ] . '"."' . $column_match[ 3 ] . '"';
+			return '`' . $this->prefix . $column_match[ 2 ] . '"."' . $column_match[ 3 ] . '`';
 		}
 
-		return '"' . $string . '"';
+		return '`' . $string . '`';
 	}
 
 	protected function column_push(&$columns)
