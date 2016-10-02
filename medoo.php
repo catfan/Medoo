a527ac1827473e58801b0e73320af037293a9722
Andrius Gecius <andrius.gecius@gmail.com>
2016-10-02 14:55:23 +0200

    changed table_qoute

--- medoo.php
+++ medoo.php
@@ -45,7 +45,7 @@ class Medoo
 
 	protected $debug_mode = false;
 
-	public function __construct($options = null, , $externalPDO = null)
+	public function __construct($options = null, $externalPDO = null)
 	{
 		if(isset($externalPDO)) {
 			$this->pdo = $externalPDO;
