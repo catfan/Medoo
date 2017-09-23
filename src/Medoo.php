705945721b02976b77eda614da86758c0ef6f1d7
Catfan <catfan.me@gmail.com>
2017-09-24 01:01:47 +0800

    [update] Improve Medoo initialization

--- src/Medoo.php
+++ src/Medoo.php
@@ -39,192 +39,190 @@ class Medoo
 
 	public function __construct($options = null)
 	{
-		try {
-			if (!is_array($options))
+		if (!is_array($options))
+		{
+			return false;
+		}
+
+		if (isset($options[ 'database_type' ]))
+		{
+			$this->database_type = strtolower($options[ 'database_type' ]);
+		}
+
+		if (isset($options[ 'prefix' ]))
+		{
+			$this->prefix = $options[ 'prefix' ];
+		}
+
+		if (isset($options[ 'option' ]))
+		{
+			$this->option = $options[ 'option' ];
+		}
+
+		if (isset($options[ 'logging' ]) && is_bool($options[ 'logging' ]))
+		{
+			$this->logging = $options[ 'logging' ];
+		}
+
+		if (isset($options[ 'command' ]) && is_array($options[ 'command' ]))
+		{
+			$commands = $options[ 'command' ];
+		}
+		else
+		{
+			$commands = [];
+		}
+
+		if (isset($options[ 'dsn' ]))
+		{
+			if (is_array($options[ 'dsn' ]) && isset($options[ 'dsn' ][ 'driver' ]))
 			{
-				return false;
+				$attr = $options[ 'dsn' ];
 			}
 
-			if (isset($options[ 'database_type' ]))
-			{
-				$this->database_type = strtolower($options[ 'database_type' ]);
-			}
-
-			if (isset($options[ 'prefix' ]))
-			{
-				$this->prefix = $options[ 'prefix' ];
-			}
-
-			if (isset($options[ 'option' ]))
-			{
-				$this->option = $options[ 'option' ];
-			}
-
-			if (isset($options[ 'logging' ]) && is_bool($options[ 'logging' ]))
-			{
-				$this->logging = $options[ 'logging' ];
-			}
-
-			if (isset($options[ 'command' ]) && is_array($options[ 'command' ]))
-			{
-				$commands = $options[ 'command' ];
-			}
-			else
-			{
-				$commands = [];
-			}
-
-			if (isset($options[ 'dsn' ]))
-			{
-				if (isset($options[ 'dsn' ][ 'driver' ]))
-				{
-					$attr = $options[ 'dsn' ];
-				}
-				else
-				{
-					return false;
-				}
-			}
-			else
-			{
-				if (
-					isset($options[ 'port' ]) &&
-					is_int($options[ 'port' ] * 1)
-				)
-				{
-					$port = $options[ 'port' ];
-				}
-
-				$is_port = isset($port);
-
-				switch ($this->database_type)
-				{
-					case 'mariadb':
-					case 'mysql':
-						$attr = [
-							'driver' => 'mysql',
-							'dbname' => $options[ 'database_name' ]
-						];
-
-						if (isset($options[ 'socket' ]))
-						{
-							$attr[ 'unix_socket' ] = $options[ 'socket' ];
-						}
-						else
-						{
-							$attr[ 'host' ] = $options[ 'server' ];
-
-							if ($is_port)
-							{
-								$attr[ 'port' ] = $port;
-							}
-						}
-
-						// Make MySQL using standard quoted identifier
-						$commands[] = 'SET SQL_MODE=ANSI_QUOTES';
-						break;
-
-					case 'pgsql':
-						$attr = [
-							'driver' => 'pgsql',
-							'host' => $options[ 'server' ],
-							'dbname' => $options[ 'database_name' ]
-						];
-
-						if ($is_port)
-						{
-							$attr[ 'port' ] = $port;
-						}
-
-						break;
-
-					case 'sybase':
-						$attr = [
-							'driver' => 'dblib',
-							'host' => $options[ 'server' ],
-							'dbname' => $options[ 'database_name' ]
-						];
-
-						if ($is_port)
-						{
-							$attr[ 'port' ] = $port;
-						}
-
-						break;
-
-					case 'oracle':
-						$attr = [
-							'driver' => 'oci',
-							'dbname' => $options[ 'server' ] ?
-								'//' . $options[ 'server' ] . ($is_port ? ':' . $port : ':1521') . '/' . $options[ 'database_name' ] :
-								$options[ 'database_name' ]
-						];
-
-						if (isset($options[ 'charset' ]))
-						{
-							$attr[ 'charset' ] = $options[ 'charset' ];
-						}
-
-						break;
-
-					case 'mssql':
-						if (strstr(PHP_OS, 'WIN'))
-						{
-							$attr = [
-								'driver' => 'sqlsrv',
-								'Server' => $options[ 'server' ] . ($is_port ? ',' . $port : ''),
-								'Database' => $options[ 'database_name' ]
-							];
-						}
-						else
-						{
-							$attr = [
-								'driver' => 'dblib',
-								'host' => $options[ 'server' ] . ($is_port ? ':' . $port : ''),
-								'dbname' => $options[ 'database_name' ]
-							];
-						}
-
-						// Keep MSSQL QUOTED_IDENTIFIER is ON for standard quoting
-						$commands[] = 'SET QUOTED_IDENTIFIER ON';
-
-						// Make ANSI_NULLS is ON for NULL value
-						$commands[] = 'SET ANSI_NULLS ON';
-						break;
-
-					case 'sqlite':
-						$this->pdo = new PDO('sqlite:' . $options[ 'database_file' ], null, null, $this->option);
-
-						foreach ($commands as $value)
-						{
-							$this->pdo->exec($value);
-						}
-
-						return;
-				}
-			}
-
-			$driver = $attr[ 'driver' ];
-
-			unset($attr[ 'driver' ]);
-
-			$stack = [];
-
-			foreach ($attr as $key => $value)
-			{
-				$stack[] = is_int($key) ? $value : $key . '=' . $value;
-			}
-
-			$dsn = $driver . ':' . implode($stack, ';');
-
+			return false;
+		}
+		else
+		{
 			if (
-				in_array($this->database_type, ['mariadb', 'mysql', 'pgsql', 'sybase', 'mssql']) &&
-				isset($options[ 'charset' ])
+				isset($options[ 'port' ]) &&
+				is_int($options[ 'port' ] * 1)
 			)
 			{
-				$commands[] = "SET NAMES '" . $options[ 'charset' ] . "'";
+				$port = $options[ 'port' ];
 			}
 
+			$is_port = isset($port);
+
+			switch ($this->database_type)
+			{
+				case 'mariadb':
+				case 'mysql':
+					$attr = [
+						'driver' => 'mysql',
+						'dbname' => $options[ 'database_name' ]
+					];
+
+					if (isset($options[ 'socket' ]))
+					{
+						$attr[ 'unix_socket' ] = $options[ 'socket' ];
+					}
+					else
+					{
+						$attr[ 'host' ] = $options[ 'server' ];
+
+						if ($is_port)
+						{
+							$attr[ 'port' ] = $port;
+						}
+					}
+
+					// Make MySQL using standard quoted identifier
+					$commands[] = 'SET SQL_MODE=ANSI_QUOTES';
+					break;
+
+				case 'pgsql':
+					$attr = [
+						'driver' => 'pgsql',
+						'host' => $options[ 'server' ],
+						'dbname' => $options[ 'database_name' ]
+					];
+
+					if ($is_port)
+					{
+						$attr[ 'port' ] = $port;
+					}
+
+					break;
+
+				case 'sybase':
+					$attr = [
+						'driver' => 'dblib',
+						'host' => $options[ 'server' ],
+						'dbname' => $options[ 'database_name' ]
+					];
+
+					if ($is_port)
+					{
+						$attr[ 'port' ] = $port;
+					}
+
+					break;
+
+				case 'oracle':
+					$attr = [
+						'driver' => 'oci',
+						'dbname' => $options[ 'server' ] ?
+							'//' . $options[ 'server' ] . ($is_port ? ':' . $port : ':1521') . '/' . $options[ 'database_name' ] :
+							$options[ 'database_name' ]
+					];
+
+					if (isset($options[ 'charset' ]))
+					{
+						$attr[ 'charset' ] = $options[ 'charset' ];
+					}
+
+					break;
+
+				case 'mssql':
+					if (isset($options[ 'driver' ]) && $options[ 'driver' ] === 'dblib')
+					{
+						$attr = [
+							'driver' => 'dblib',
+							'host' => $options[ 'server' ] . ($is_port ? ':' . $port : ''),
+							'dbname' => $options[ 'database_name' ]
+						];
+					}
+					else
+					{
+						$attr = [
+							'driver' => 'sqlsrv',
+							'Server' => $options[ 'server' ] . ($is_port ? ',' . $port : ''),
+							'Database' => $options[ 'database_name' ]
+						];
+					}
+
+					// Keep MSSQL QUOTED_IDENTIFIER is ON for standard quoting
+					$commands[] = 'SET QUOTED_IDENTIFIER ON';
+
+					// Make ANSI_NULLS is ON for NULL value
+					$commands[] = 'SET ANSI_NULLS ON';
+					break;
+
+				case 'sqlite':
+					$this->pdo = new PDO('sqlite:' . $options[ 'database_file' ], null, null, $this->option);
+
+					foreach ($commands as $value)
+					{
+						$this->pdo->exec($value);
+					}
+
+					return;
+			}
+		}
+
+		$driver = $attr[ 'driver' ];
+
+		unset($attr[ 'driver' ]);
+
+		$stack = [];
+
+		foreach ($attr as $key => $value)
+		{
+			$stack[] = is_int($key) ? $value : $key . '=' . $value;
+		}
+
+		$dsn = $driver . ':' . implode($stack, ';');
+
+		if (
+			in_array($this->database_type, ['mariadb', 'mysql', 'pgsql', 'sybase', 'mssql']) &&
+			isset($options[ 'charset' ])
+		)
+		{
+			$commands[] = "SET NAMES '" . $options[ 'charset' ] . "'";
+		}
+
+		try {
 			$this->pdo = new PDO(
 				$dsn,
 				$options[ 'username' ],
@@ -238,7 +236,7 @@ class Medoo
 			}
 		}
 		catch (PDOException $e) {
-			throw new Exception($e->getMessage());
+			throw new PDOException($e->getMessage());
 		}
 	}
 
