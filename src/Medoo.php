6821eb6f9b2b587ada3bfc5df58d022f6794476e
Catfan <catfan.me@gmail.com>
2017-12-20 03:38:43 +0800

    [fix] Fix #690 incorrect AND and OR key detection on where clause

--- src/Medoo.php
+++ src/Medoo.php
@@ -521,19 +521,17 @@ class Medoo
 		{
 			$type = gettype($value);
 
-			if ($type === 'array')
+			if (
+				$type === 'array' &&
+			)
 			{
-				$relationship = strpos($key, 'AND', 0) !== false ? 'AND' :
-				 				(strpos($key, 'OR', 0) !== false ? 'OR' : false);
+				$relationship = $relation_match[ 1 ];
 
-				 if ($relationship)
-				 {
-					$stack[] = !empty(array_diff_key($value, array_keys(array_keys($value)))) ?
-						'(' . $this->dataImplode($value, $map, ' ' . $relationship) . ')' :
-						'(' . $this->innerConjunct($value, $map, ' ' . $relationship, $conjunctor) . ')';
+				$stack[] = !empty(array_diff_key($value, array_keys(array_keys($value)))) ?
+					'(' . $this->dataImplode($value, $map, ' ' . $relationship) . ')' :
+					'(' . $this->innerConjunct($value, $map, ' ' . $relationship, $conjunctor) . ')';
 
-					continue;
-				}
+				continue;
 			}
 
 			$map_key = $this->mapKey();
