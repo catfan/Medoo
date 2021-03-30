4b12be61610d09e5a3857b29e6b00064f1b7434b
Angel <angel.lai.cat@gmail.com>
2021-03-30 18:16:24 +0800

    [feature] Medoo test case

---
+++ tests/MedooTestCase.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace Medoo\Tests;
+
+use Medoo\Medoo;
+use PHPUnit\Framework\TestCase;
+
+class MedooTestCase extends TestCase
+{
+    protected Medoo $database;
+
+    public function setUp() : void
+    {
+        $this->database = new Medoo([
+            'testMode' => true
+        ]);
+    }
+
+    public function typesProvider(): array
+    {
+        return [
+            'MySQL' => ['mysql'],
+            'MSSQL' => ['mssql'],
+            'SQLite' => ['sqlite'],
+            'PostgreSQL' => ['pgsql'],
+            'Oracle' => ['oracle']
+        ];
+    }
+
+    public function setType($type) : void
+    {
+        $this->database->type = $type;
+    }
+
+    public function expectedQuery($expected) : string
+    {
+        $identifier = [
+            'mysql' => '`$1`',
+            'mssql' => '[$1]'
+        ];
+
+        return preg_replace(
+            '/"((?![_\d])[\p{N}\p{L}_]+)"/u',
+            $identifier[$this->database->type] ?? '"$1"',
+            $expected
+        );
+    }
+
+    public function assertQuery($expected, $query) : void
+    {
+        if (is_array($expected)) {
+            
+            $this->assertEquals(
+                $this->expectedQuery($expected[$this->database->type] ?? $expected['default']),
+                $query
+            );
+        }
+        else {
+            $this->assertEquals($this->expectedQuery($expected), $query);
+        }
+    }
+}
