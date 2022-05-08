e5b44862b22d8b571b0958f2588c33d7ccfeb469
Angel <angel.lai.cat@gmail.com>
2022-05-09 04:43:54 +0800

    [update] Add test case for #1033

--- tests/DropTest.php
+++ tests/DropTest.php
@@ -2,6 +2,8 @@
 
 namespace Medoo\Tests;
 
+use Medoo\Medoo;
+
 /**
  * @coversDefaultClass \Medoo\Medoo
  */
@@ -22,6 +24,28 @@ class DropTest extends MedooTestCase
             DROP TABLE IF EXISTS "account"
             EOD,
             $this->database->queryString
+        );
+    }
+
+    /**
+     * @covers ::drop()
+     */
+    public function testDropWithPrefix()
+    {
+        $database = new Medoo([
+            'testMode' => true,
+            'prefix' => 'PREFIX_'
+        ]);
+
+        $database->type = "sqlite";
+
+        $database->drop("account");
+
+        $this->assertQuery(
+            <<<EOD
+            DROP TABLE IF EXISTS "PREFIX_account"
+            EOD,
+            $database->queryString
         );
     }
 }
