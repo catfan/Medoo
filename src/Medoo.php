86ac2b376303f6c3c8e915be9051599b67a8a09e
Angel <angel.lai.cat@gmail.com>
2022-05-10 10:47:28 +0800

    [release] Medoo v2.1.6

--- src/Medoo.php
+++ src/Medoo.php
@@ -6,7 +6,7 @@ declare(strict_types=1);
  *
  * The Lightweight PHP Database Framework to Accelerate Development.
  *
- * @version 2.1.5
+ * @version 2.1.6
  * @author Angel Lai
  * @package Medoo
  * @copyright Copyright 2022 Medoo Project, Angel Lai.
